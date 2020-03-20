009cb6132a05eb1ef5440a7a7866275815be17b4
jhonrs <jhonrs812@gmail.com>
2020-03-20 00:18:01 -0500

    Variables de precio por mes (No se define el valor del precio)

--- scripts/Logica.js
+++ scripts/Logica.js
@@ -1,20 +1,61 @@
-class Logica{
+class Logica {
 
-    constructor(app){
-        this.app = app;   
+    constructor(app) {
+        this.app = app;
+        this.precio;
+
+        this.valor;
+
+        switch (precio) {
+            case Enero:
+                valor = 50;
+                break;
+
+            case Febrero:
+                valor = 0;
+                break;
+
+            case Marzo:
+                valor = 0;
+                break;
+
+            case Abril:
+                valor = 0;
+                break;
+
+            case Mayo:
+                valor = 0;
+                break;
+
+            case Junio:
+                valor = 0;
+                break;
+
+            case Julio:
+                valor = 0;
+                break;
+
+            case Agosto:
+                valor = 0;
+                break;
+
+            case Septiembre:
+                valor = 0;
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+    mouse() {
 
     }
 
-  
 
-    mouse(){
-       
-    }
-
-    
-    pintar(){
-        this.app.background(255,200,200);
-
+    pintar() {
+        this.app.background(255, 200, 200);
     }
 }
 
